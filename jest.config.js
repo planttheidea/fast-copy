316e3649de4f318caae9b90f98668cbeccbc52ec
Tony Quetano <tony.quetano@planttheidea.com>
2022-09-23 06:43:44 -0400

    fix jest config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
-module.exports = {
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  roots: ["<rootDir>"],
-  testRegex: "/__tests__/.*\\.(ts|tsx)$",
+export default {
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  roots: ['<rootDir>'],
+  testRegex: '/__tests__/.*\\.(ts|tsx)$',
   transform: {
-    "\\.(ts|tsx)$": "ts-jest"
+    '\\.(ts|tsx)$': 'ts-jest',
   },
-  verbose: true
+  verbose: true,
 };
